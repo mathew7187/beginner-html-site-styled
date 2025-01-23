2482800dc5458ab3c061cf2340b26a7804466c43
mathew7187 <mathew.medina24@student.cart.org>
2025-01-23 14:49:42 -0800

    javascript

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+
+let myVariable;
+myVariable = "Bob";
+console.log(num);
+
+myVariable;
+let myVariable = "Bob";
+myVariable = "Steve";
+console.log(num);
+
+/*
+Everything in between is a comment.
+*/
+
+// This is a comment
+
+const myImage = document
